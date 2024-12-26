ffe333a24034c51e923a639625a4af68408d0e45
пк <@DESKTOP-81SKH73>
2024-12-26 17:52:49 +0200

    Добавьте файлы проекта.

---
+++ ClassLibrary1/ClassLib.cs
@@ -0,0 +1,20 @@
+﻿namespace ClassLab6_2
+{
+    public class ClassLib
+    {
+        public static int[] Sum(int[,] mas)
+        {
+            int[] sumColum = new int[mas.GetLength(1)];
+            for (int j = 0; j < mas.GetLength(1); j++)
+            {
+                int s = 0;
+                for (int i = 0; i < mas.GetLength(0); i++)
+                {
+                    s += mas[i, j];
+                }
+                sumColum[j] = s;
+            }
+            return sumColum;
+        }
+     }
+}
---
+++ lab6.2/Program.cs
@@ -0,0 +1,18 @@
+﻿﻿using static ClassLab6_2.ClassLib;
+Random random = new Random();
+int[,] mas = new int[4, 5];
+for (int i = 0; i < mas.GetLength(0); i++)
+{
+	for (int j = 0; j < mas.GetLength(1); j++)
+	{
+		mas[i, j] = random.Next(10, 100);
+		Console.Write(mas[i, j] + " ");
+	}
+	Console.WriteLine();
+}
+Console.WriteLine("Сумма столбцов");
+int[] vector = Sum(mas);
+for (int i = 0; i < vector.Length; i++)
+{
+    Console.Write(vector[i] + " ");
+}
